3f43c5d159cfbc638f189b1e5d288b94f3dff5fd
Eduardo J. Provin <79066522+EduardoJPMarcelino@users.noreply.github.com>
2025-04-28 21:41:42 -0300

    Fixed error in script test eavesdropping

--- test_bb84.py
+++ test_bb84.py
@@ -11,7 +11,7 @@ class TestBB84(unittest.TestCase):
         alice_key = np.array([0, 1, 0])
         bob_key = np.array([0, 1, 0])
         result = check_for_eavesdropping(alice_key, bob_key, 2)
-        self.assertFalse(result, "Same keys")
+        self.assertFalse(result, "Same keys detected:")
 
 if __name__ == "__main__":
     unittest.main()
